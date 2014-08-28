a8ccb346ab27dba0ae11fc0f19412dffea60f553
Michele Schimd <michele.schimd@gmail.com>
2014-08-28 16:43:46 +0200

    added parameters parsing to qualityValueStats.py

--- src/python/qualityValueStats.py
+++ src/python/qualityValueStats.py
@@ -1,24 +1,34 @@
 #! /usr/bin/python
 
-def printUsage():
-    print """Usage:
-   qualityValueStats <fastq_file>
-"""
 
 from Bio import SeqIO
 import sys
+import argparse
+
+def parseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="input fastq file")
+    parser.add_argument("-o", "--output", help="output file")
+    parser.add_argument("-q", "--quality", help="maximum quality score considered (minimum is always 0)", type=int, default=100)
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        printUsage()
-        sys.exit(1)
-    fileName = sys.argv[1]
+    args = parseArguments()
+    fileName =  args.input
+    outFile =  args.output
+    maxQuality = args.quality
     fileHandle = open(fileName, "rU")
-    qualsVector = [0]*100
+    qualsVector = [0]*maxQuality
     for record in SeqIO.parse(fileHandle, "fastq"):
         temp = record.letter_annotations["phred_quality"]
         for x in temp:
             qualsVector[int(x)] += 1
+    outFileHandle = None
+    if outFile != None:
+        outFileHandle = open(outFile, "w")
     for q in qualsVector:
-        print(q)
-    
+        if outFileHandle == None:
+            print(q)
+        else:
+            outFileHandle.write(str(q) + "\n")
