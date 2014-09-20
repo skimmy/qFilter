7743db5eb70131c4a750d610835df586d9c30822
Michele Schimd <skimmy@chakra>
2014-09-20 15:41:25 +0200

    minor bugfix

--- src/c/util/stxxl_helper.hpp
+++ src/c/util/stxxl_helper.hpp
@@ -2,10 +2,11 @@
 #define _STXXL_HELPER_H_
 
 #include <iostream>
+#include <fstream>
 
 #include <stxxl/sorter>
 
-#include "seq.h"
+#include "seq.hpp"
 #include "../read_sorter.hpp"
 
 #define DEFAULT_BLOCK_SIZE 4096
@@ -16,7 +17,7 @@ typedef stxxl::sorter<read_record_t, ReadRecordComparator, DEFAULT_BLOCK_SIZE> r
 /**
  * \brief sort fastq file using stxxl external sorting
  */
-void sortFastqReadStxxl(std::ifstream & input, std::ofstrem& sorted) {
+void sortFastqReadStxxl(std::ifstream & input, std::ofstream& sorted) {
   FastqRead r;
   
   read_record_sorter sorter(ReadRecordComparator(), DEFAULT_MEM_SIZE);
