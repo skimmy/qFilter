df8ebade9590ea7f6b420c768717a945068dfee0
skimmy <michele.schimd@gmail.com>
2014-09-28 14:16:37 +0200

    Added few more scripts stub

---
+++ src/scripts/sequencingExperiment.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# This script simulates an entire sequencing experiment. It starts from a given
+# reference sequence (as a fast format) and several experiment parameters:
+#     length of the sequence (required)
+#     length of the read (default 50)
+#     number of reads or coverage (default 10000 / 10)
+#     quality distribution or sample fastq (default is uniform qualities [0,40])
