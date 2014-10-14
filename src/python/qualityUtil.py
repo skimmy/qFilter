322545c71552fd92673fae1bc2b19b252ebe6370
Michele Schimd <michele.schimd@gmail.com>
2014-10-14 11:54:16 +0200

    Bugfix: wrong read probability calculation (quality utils)

--- src/python/qualityUtil.py
+++ src/python/qualityUtil.py
@@ -8,12 +8,6 @@ def valueToProb(q):
 def valuesToProbIID(quals):
     p = 1.0
     for q in quals:
-        p = p * valueToProb(q)
-    return p
-
-def valuesToErrorProbIID(quals):
-    p = 1.0
-    for q in quals:
         p = p * (1.0 - valueToProb(q))
     return p
 
@@ -23,8 +17,7 @@ def valuesToErrorProbIID(quals):
 if __name__ == "__main__":
     for q in range(41):
         print(str(valueToProb(q)))
-    quals = [30, 30, 22, 21, 27, 23]
+    quals = [33, 34, 34, 34, 34, 32, 34, 34, 33, 32, 24, 33, 34, 33, 32, 34, 33, 34, 16, 27, 31, 32, 32, 20, 33, 28, 32, 25, 26, 31, 21, 18, 26, 29, 26, 34]
     print("\n\n")
     print(valuesToProbIID(quals))
-    print(valuesToErrorProbIID(quals))
 
