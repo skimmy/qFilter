bc463d0f046361f097164531e68dabb7dbfd3e07
Michele Schimd <michele.schimd@gmail.com>
2015-01-27 10:36:57 +0100

    entropy calculator completed

--- src/python/entropyCalculator.py
+++ src/python/entropyCalculator.py
@@ -15,14 +15,26 @@ def parseArguments():
 A = float(math.log(3,2))
 
 def alpha(q):
+    if q == 0:
+        return 0
     p = float(qu.valueToProb(q))
-    #    x = ( (1.0 - p)  / (math.log( (1.0 - p), 2)) )# + ( (p) / (math.log(p,2) + A) )
-    x = float((1.0 - p) / float(math.log(1.0 - p,2)) + ( (p) / (math.log(p,2) + A) ))
+    q = float(1.0 - p)
+    x = A - p * math.log(p,2) - q * math.log(q,2)
     return x
+
+def loadQualityDistribution(distFile):
+    fh = open(distFile, "rU")
+    Q = []
+    for line in fh:
+        Q.append(float(line))
+    tot = sum(Q)
+    F = [x / float(tot) for x in Q]         
+    return F
 
 if __name__ == "__main__":
     args = parseArguments()
-    Q = range(1,30)
+    distFile = args.distribution
+    Q = loadQualityDistribution(distFile)
     pq = 1.0 / float(len(Q))
     H1 = 0.0
     H2 = 0.0
@@ -31,6 +43,7 @@ if __name__ == "__main__":
         H1 = H1 + pq * alpha(q)
         tildeP = tildeP + pq * qu.valueToProb(q)    
         #print"%d  %f %f " % (q,qu.valueToProb(q),alpha(q))
-    H2 = ( (1.0 - tildeP) / math.log(1.0 - tildeP) ) + ( tildeP / (math.log(tildeP,2) + A ) )
-    print "%f %f" % (H1, H2)
-    print pq
+    tildeQ = 1.0 - tildeP
+    H2 = A - tildeP * math.log(tildeP,2) - tildeQ * math.log(tildeQ, 2)
+    print "%f %f %f" % (H1, H2, math.fabs(H1 - H2))
+
